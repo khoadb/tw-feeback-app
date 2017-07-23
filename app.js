9ab20c6c1c81bcac6a2af164570ddbc9e038cb28
Khoa Do <khoa.do.bui@gmail.com>
2017-07-23 16:49:17 +0700

    clean up code

--- app.js
+++ app.js
@@ -101,120 +101,8 @@ mainApp.controller('welcomeCtrl', function($rootScope, $scope, $location) {
  //     $location.path("/showVideo"); 
   // },3000);
 })
-
-
-// // Simple GET request example:
-// $http({
-//   method: 'GET',
-//   url: '/someUrl'
-// }).then(function successCallback(response) {
-//     // this callback will be called asynchronously
-//     // when the response is available
-//   }, function errorCallback(response) {
-//     // called asynchronously if an error occurs
-//     // or server returns response with an error status.
-//   });
-
-
-mainApp.controller('BalanceController', function($rootScope, $scope, $routeParams, $location, $timeout, WCFService, testWCFService) {
-	
-	$scope.message = "This the balance controller.";
-	//WCFService.getCardInfo();
-
-	//reset scope values
-	$scope.customer_info = {};
-	$scope.customer_trans = {};
-
-	//khd, get the params:
-	var param1 = $routeParams.card_id;
-	console.log("Paramenter are: ", param1);
-
-
-    //Get list of transactions
-	testWCFService.getCustTrans(param1)
-        .then( function(response) {
-          console.log('getCustTrans - Raw data: ', response);
-          console.log('Data: ', response.data);
-          console.log('Data.lenght: ', response.data.length);
-          $scope.customer_trans = response.data;
-        }, function(response) {
-        	console.log("error...");
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        //console.log('nfcUseCasesService response with an error', status);
-        });
-
-    //get customer information
- 	testWCFService.getCustInfo(param1)
-        .then( function(response) {
-          console.log('getCustInfo - Raw data: ', response);
-          console.log('Data: ', response.data);
-          console.log('Data.lenght: ', response.data.length);
-          $scope.customer_info = response.data;
-        }, function(response) {
-        	console.log("error...");
-        // called asynchronously if an error occurs
-        // or server returns response with an error status.
-        //console.log('nfcUseCasesService response with an error', status);
-        });       
-
-
-
-    //finally set status to 'ready'
-    $rootScope.status = 'ready';
-
-
-	var todoList = this;
-	//var todoList = {};
-    todoList.todos = [
-      {text:'learn AngularJS', done:true},
-      {text:'build an AngularJS app', done:false}];
  
-
-    console.log("Show Balacne..!");
-
-
-    //test timer 
-   //  $timeout(function(){ 
-  	// 	logged_in = true;
-  	// 	$location.path("/landingPage"); 
-	  // },15000);
-
-})
-
-
-mainApp.controller('VideoController', function($rootScope, $scope, $location, $timeout, WCFService) {
-	
-	$scope.message = "This the Show Video controller.";
-
-	//WCFService.getCardInfo();
-
-	var todoList = this;
-	//var todoList = {};
-    todoList.todos = [
-      {text:'learn AngularJS', done:true},
-      {text:'build an AngularJS app', done:false}];
- 
-
-    console.log("Show Video..!");
-
-    // setTimeout(function() {	
-    //  	$location.path('/showBalance');
-    //  	//$location.path("/");
-    //  	console.log('timeer: '); 
-    //  	console.log($location.path());
-    //  }, 5000);
-
-
-    $timeout(function(){ 
-  		logged_in = true;
-  		$location.path("/showBalance"); 
-	},10000);
-
-    console.log('end timeer: ');  
-    
-
-})
+  
 
 //khd: 19.07.2017
 mainApp.controller('VotedController', function ($rootScope, $scope, $location, $timeout, WCFService, votingService) {
@@ -405,33 +293,6 @@ mainApp.filter('reverse', function() {
 })
 
 
-
-//khd, add digits separator, i.e. 1000 -> 1,000
-mainApp.filter('currency_digits', function() {
-  return function(input, uppercase) {
-    //console.log("input: ", input, uppercase); 
-
-    input = input || '';
-    var out = '';
-
-    out = input.toLocaleString();
-    //return input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-    //out = input.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-
-    // for (var i = 0; i < input.length; i++) {
-    //   out = input.charAt(i) + out;
-    // }
-    // // conditional based on optional argument
-    // if (uppercase) {
-    //   out = out.toUpperCase();
-    // }
-
-    return out;
-
-  };
-})
-
-
 mainApp.service('WCFService', function () {
   this.getCardInfo = function() {
   	//var filename = path.join(__dirname, '/assets/test.json')
@@ -449,16 +310,6 @@ mainApp.service('WCFService', function () {
 });
 
 
-mainApp.service('nfcPhonesService', function () {
-  this.getPhones = function() {
-  	var filename = path.join(__dirname, '/assets/test.json')
-    console.log('filename: ', filename);
-    var mydata = JSON.parse(filename);
-    console.log(mydata);
-    //return mydata;
-  };
-
-});
 
 mainApp.service('testWCFService', function ($http) {
   this.getCustInfo = function(card_id) {
