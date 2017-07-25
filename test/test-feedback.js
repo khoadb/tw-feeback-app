3f7e2fc922fd8eb585e943ddbf8aaf534bec0ea4
Khoa Do <khoa.do.bui@gmail.com>
2017-07-25 17:52:41 +0700

    testing sqlite

--- test/test-feedback.js
+++ test/test-feedback.js
@@ -1,19 +1,24 @@
 var sqlite3 = require('sqlite3').verbose();
 //var db = new sqlite3.Database(':memory:');
-var db = new sqlite3.Database('tw-feedback.db');
+//var db = new sqlite3.Database('tw-feedback.db');
+let db = new sqlite3.Database('tw-feedback.db');
 
-db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
+db.run('CREATE TABLE feedbacks(name text)');
 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
-  stmt.finalize();
 
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-      console.log(row.id + ": " + row.info);
-  });
-});
+// db.serialize(function() {
+//   db.run("CREATE TABLE IF NOT EXISTS feedbacks (the_id integer, card_number text, rated_score int, rated_name text, location_code text, location_name text, date_created text )" );
+
+//   var stmt = db.prepare("INSERT INTO feedbacks VALUES (?)");
+//   for (var i = 0; i < 10; i++) {
+//       stmt.run(i, "cardnumber_" + i, 3, "Good", "TW007", "TW Saigon Center", "date_time" );
+//   }
+//   stmt.finalize();
+
+//   db.each("SELECT rowid AS id, info FROM feedbacks", function(err, row) {
+//       console.log(row.id + ": " + row.info);
+//   });
+
+// });
 
 db.close();
