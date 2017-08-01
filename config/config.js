16ed7b941960a9e8a74f51bd384a2cf12d360580
Khoa Do <khoa.do.bui@gmail.com>
2017-08-01 14:30:14 +0700

    update location code and location name list.

--- config/config.js
+++ config/config.js
@@ -6,5 +6,38 @@ var config = module.exports = {};
 config.survey_question = 'Cho chúng tôi biết cảm nhận của bạn về tiNiWorld'  
  
 //Center information
-config.location_code = 'TW_011';
-config.location_name = 'TW Vincom Thao Dien';
+config.location_code = 'tT014';
+config.location_name = 'TW Saigon Center';
+
+
+
+
+// location_code, location_name (as of 01. August 2017)
+// -------------------------------------
+// tT001	Trung tâm Aeon Mall Tân Phú
+// tT003	Trung tâm Aeon Long Biên
+// tT004	Trung tâm Vincom NC Thanh
+// tT005	Trung tâm Vincom Việt Trì
+// tT006	Trung tâm Vincom An Giang
+// tT007	Trung tâm tT Vincom Thảo Điền
+// tT008	Trung tâm Vincom Hải Phòng
+// tT009	Trung tâm Vincom Quang Trung
+// tT010	Trung tâm Vincom Biên Hòa
+// tT011	Trung tâm Vincom Cần Thơ
+// tT012	Trung tâm Vincom Lê Văn Việt
+// tT013	Trung tâm Aeon Bình Tân
+// tT014	Trung tâm Saigon Center
+// tW002	Trung tâm Nowzone
+// tW004	TT CoopMart Lý Thường Kiệt
+// tW006	Trung tâm Syrena Hà Nội
+// tW008	Trung tâm Citi Plaza Tân Bình
+// tW009	Trung Tâm Mipec
+// tW010	TT CoopMart Buôn Mê Thuột
+// tW014	Trung tâm IPH Hà Nội
+// tW017	Trung tâm BigC Pandora
+// tW018	TT MarinePlaza HạLongQuảngNinh
+// tW026	Trung tâm Vincom Thủ Đức
+// tW027	Trung Tâm Hồ Gươm
+// tW028	Trung tâm Lotte Mart Cần Thơ
+// tW029	Trung tâm Satra Phạm Hùng
+// tW030	Trung Tâm BigC Thăng Long
