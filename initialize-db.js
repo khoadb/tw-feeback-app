cfed98d41c797882c34d37ba7477033b7840e69a
Khoa Do <khoa.do.bui@gmail.com>
2017-08-04 14:07:51 +0700

    modified db name to be dynamic based on center_code when creted.

--- initialize-db.js
+++ initialize-db.js
@@ -16,6 +16,9 @@
 */ 
 
 var sqlite3 = require('sqlite3').verbose();
-let db = new sqlite3.Database('db/tw-feedback.db');
+var cfg = require('./config/config'); 
+//let db = new sqlite3.Database('db/tw-feedback.db');
+var db_filename =  'db/tw-feedback_'+ cfg.location_code + '.db'; 
+var db = new sqlite3.Database(db_filename);
 db.run("CREATE TABLE IF NOT EXISTS feedbacks (survey_question text, card_number text, rated_score int, rated_name text, location_code text, location_name text, date_created integer, date_string text)" );
 db.close();
